c2c9081c108060379866d88432bd7e969d15fa26
unknown <yoohj367@gmail.com>
2020-12-17 16:03:46 +0900

    addinfo

---
+++ src/main/java/com/bs/dabom/controller/AddInfo_Controller.java
@@ -0,0 +1,16 @@
+package com.bs.dabom.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.bs.dabom.model.biz.AddInfo_Biz;
+
+@Controller
+public class AddInfo_Controller {
+	
+	@Autowired
+	private AddInfo_Biz biz;
+	
+	
+
+}
